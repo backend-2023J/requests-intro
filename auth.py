b0ea35942406691534f50992455bd911b1cd0dba
JavohirJalilov <javohirjaliloff@gmail.com>
2023-09-16 17:42:43 +0500

    get crypto address

--- auth.py
+++ auth.py
@@ -1,10 +1,15 @@
 import requests
 
-url = "https://randommer.io/api/Card"
+url = "https://randommer.io/api/Finance/CryptoAddress"
 
 headers = {
     "X-Api-Key": "f1ab06cd2da14928a4f4299e85162d76"
 }
-response = requests.get(url, headers=headers)
+parameters = {
+    "cryptoType": "Bitcoinplus"
+}
+response = requests.get(url, params=parameters, headers=headers)
 
 print(response.json())
+
+# ['Argoneum', 'BCash', 'BGold', 'BitCore', 'Bitcoin', 'Dash', 'Dogecoin', 'Verge', 'Dystem', 'Feathercoin', 'Groestlcoin', 'Liquid', 'Litecoin', 'Monacoin', 'Polis', 'Terracoin', 'UFO', 'Viacoin', 'Zclassic', 'Koto', 'Bitcoinplus', 'Chaincoin', 'ZCoin', 'DogeCash', 'Ethereum']
